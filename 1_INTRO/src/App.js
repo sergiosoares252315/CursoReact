59a732f7a3c38a43b4c8c5bf6187c8a694d348dd
Sérgio Soares <ser.vit.deb@gmail.com>
2025-10-31 14:15:48 -0300

    Aula 1: Introdução

---
+++ 1_INTRO/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App-header">
+      <h1 className="texto-amarelo">Hello World React!</h1>
+      <h2 className='texto-turquesa'>Emmet funcionou</h2>
+      <h3 className="texto-vermelho">Elemento do desafio!</h3>
+      </div>
+  );
+}
+
+export default App;

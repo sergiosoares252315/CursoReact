3b697e15e7b52abe2116ac58666d1a3369ea98d6
Sérgio Soares <ser.vit.deb@gmail.com>
2025-11-06 19:31:59 -0300

    Aula: 6 Project Scret Word

---
+++ 6_SECRET_WORD/secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Embreagem"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacate"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Dedos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Memória"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Csharp"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Batata"],
+};
